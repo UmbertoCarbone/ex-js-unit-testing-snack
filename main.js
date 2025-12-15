8e185dd44c92238e41d765b32942702efa7abc26
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-12-15 20:36:13 +0100

    snack fino al 7 + challenge

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
 function getInitials(fullName) {
-  return fullName
-    .split(" ")
-    .map(word => word[0].toUpperCase())
-    .join('.') + '.';
+  return (
+    fullName
+      .split(" ")
+      .map((word) => word[0].toUpperCase())
+      .join(".") + "."
+  );
 }
 function createSlug(str) {
-  return str.toLowerCase();
+  if (!str) {
+    throw new Error("valore vuoto");
+  }
+  return str.toLowerCase().replaceAll(" ", "-");
 }
 
 function average(arr) {
+  arr.forEach((numero) => {
+    if (isNaN(numero) || null || undefined) {
+      throw new Error("average vuole solo numeri");
+    }
+  });
   const sum = arr.reduce((acc, num) => acc + num, 0);
   return sum / arr.length;
 }
-
-module.exports = {
-    getInitials,
-    createSlug,
-    average,
+function isPalindroma(word) {
+  const reversed = word.split("").reverse().join("");
+  if (word !== reversed) {
+    throw new Error("la parola non e palindroma");
+  }
+  return word;
 }
+
+function findPostById(posts, id) {
+  if (isNaN(id)){
+    throw new Error("non e un id");
+  }
+  return posts.find((p) => p.id === id);
+}
+module.exports = {
+  getInitials,
+  createSlug,
+  average,
+  isPalindroma,
+  findPostById,
+};
--- test.test.js
+++ test.test.js
@@ -1,29 +1,85 @@
-const { getInitials,createSlug,average } = require("./main.js");
+const {
+  getInitials,
+  createSlug,
+  average,
+  isPalindroma,
+  findPostById,
+} = require("./main.js");
 
+describe("manipolazioni di stringhe", () => {
+  test("restituire le iniziali di un nome completo con un punto", () => {
+    console.log(getInitials("Mario Rossi"));
+    expect(getInitials("Mario Rossi")).toBe("M.R.");
+    expect(getInitials("Luigi Verdi")).toBe("L.V.");
+  });
+  test("verificare se la stringa e' palindroma", () => {
+    expect(isPalindroma("otto")).toBe("otto");
+    expect(isPalindroma("anna")).toBe("anna");
+    expect(() => isPalindroma("ciao")).toThrow();
+  });
+});
+
+describe("operazione su array", () => {
+  test("calcolare la media di un array di numeri", () => {
+    expect(average([15, 5])).toBe(10);
+    expect(() => average([7, "ciao"])).toThrow();
+  });
+  const posts = [
+    { id: 1, title: "Primo post", slug: "primo-post" },
+    { id: 2, title: "Secondo post", slug: "secondo-post" },
+    { id: 3, title: "Terzo post", slug: "terzo-post" },
+  ];
+
+  test("Ogni post ha le proprietà id, title e slug e l'id è numerico", () => {
+    expect(findPostById(posts, 2)).toEqual({
+      id: 2,
+      title: "Secondo post",
+      slug: "secondo-post",
+    });
+    expect(() => findPostById(posts, "prova")).toThrow();
+  });
+});
+
+describe("generazioni di slug", () => {
+  test("test per verifica una stringa in lowercase", () => {
+    expect(createSlug("Ciao")).toBe("ciao");
+  });
+  test("la funzione sostituisce gli spazi con: -", () => {
+    expect(createSlug("Questo e UN tEst")).toBe("questo-e-un-test");
+  });
+  test("la funzione createSlug lancia un errore se il titolo e vuoto o non valido", () => {
+    expect(() => createSlug("")).toThrow();
+    expect(() => createSlug(null)).toThrow();
+  });
+});
 /* 🏆 Snack 1 */
 /* Creare un test che verifichi la seguente descrizione:
 👉 "La funzione getInitials restituisce le iniziali di un nome completo." */
-
-test("restituire le iniziali di un nome completo con un punto", () => {
-  console.log(getInitials("Mario Rossi"));
-  expect(getInitials("Mario Rossi")).toBe("M.R.");
-  expect(getInitials("Luigi Verdi")).toBe("L.V.");
-});
 
 /* 🏆 Snack 2 */
 /* Creare un test che verifichi la seguente descrizione:
 👉 "La funzione createSlug restituisce una stringa in lowercase." */
 
-test("test per verifica una stringa in lowercase", () => {
-  expect(createSlug("Ciao")).toBe("ciao");
-});
-
-
 /* 🏆 Snack 3 */
 /* Creare un test che verifichi la seguente descrizione:
 👉 "La funzione average calcola la media aritmetica di un array di numeri." */
 
-test("calcolare la media di un array di numeri",() =>{
-    expect(average([15,5])).toBe(10)
-})
-.
+/* 🏆 Snack 4 */
+/* Creare un test che verifichi la seguente descrizione:
+👉 "La funzione createSlug sostituisce gli spazi con -." */
+
+/* 🏆 Snack 5
+Creare un test che verifichi la seguente descrizione:
+👉 "La funzione isPalindrome verifica se una stringa è un palindromo.*/
+
+/* 🏆 Snack 6
+Creare un test che verifichi la seguente descrizione:
+👉 "La funzione createSlug lancia un errore se il titolo è vuoto o non valido." */
+
+/* 🏆 Snack 7 */
+/* Crea un array di oggetti posts, in cui ogni oggetto ha le proprietà id, title e slug.
+
+Creare un test che verifichi le seguenti descrizioni:
+
+👉 "La funzione findPostById restituisce il post corretto
+ dato l’array di post e l’id" */
